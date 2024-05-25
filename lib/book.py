e8a6a8ecfc185ac52046c92e46464a08af750571
mendarrr <sarahabby541@gmail.com>
2024-05-25 18:37:53 +0300

    Attempted lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-
 class Book:
+    def __init__(self, title, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+            self.page_count = 0
+        else:
+            self.page_count = page_count
+        self.title = title
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")   
     pass
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+            self.size = 0
+        else:
+            self.size = size
+        self.brand = brand
+        self.condition = "New"
+
+    def cobble(self):
+        self.condition = "New"
+        print("The shoe has been repaired.")   
     pass
